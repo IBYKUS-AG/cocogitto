f7079c916f30afebeb2dcc1c04ab1e4c1cdb269e
Lindner, Bernhard <bernhard.lindner@ibykus.de>
2026-05-12 17:21:41 +0200

    test(changelog): add test for custom template with macros

--- crates/cocogitto/tests/cog_tests/changelog.rs
+++ crates/cocogitto/tests/cog_tests/changelog.rs
@@ -483,6 +483,82 @@ fn get_changelog_with_custom_template() -> Result<()> {
 }
 
 #[sealed_test]
+fn get_changelog_with_custom_template_using_macros() -> Result<()> {
+    // Arrange
+    let crate_dir = env!("CARGO_MANIFEST_DIR");
+    let template = PathBuf::from(crate_dir).join("tests/cog_tests/template_macros.md");
+
+    git_init()?;
+
+    let cog_toml = indoc!(
+        "[changelog]
+        remote = \"github.com\"
+        repository = \"test\"
+        owner = \"test\""
+    );
+
+    run_cmd!(echo $cog_toml > cog.toml;)?;
+    let init = git_commit("chore: init")?;
+    let commit_one = git_commit("feat(scope1): start")?;
+    let commit_two = git_commit("feat: feature 1")?;
+    git_tag("1.0.0")?;
+    let commit_three = git_commit("feat: feature 2")?;
+    let commit_four = git_commit("fix: bug fix 1")?;
+    let commit_five = git_commit("chore(version): 2.0.0")?;
+    git_tag("2.0.0")?;
+
+    // Act
+    let changelog = Command::new(assert_cmd::cargo_bin!("cog"))
+        .arg("changelog")
+        .arg("-t")
+        .arg(template)
+        // Assert
+        .assert()
+        .success();
+
+    let changelog = changelog.get_output();
+    let changelog = String::from_utf8_lossy(&changelog.stdout);
+    let today = Utc::now().date_naive();
+
+    assert_doc_eq!(
+        changelog.as_ref(),
+        "## [2.0.0](https://github.com/test/test/compare/1.0.0..2.0.0) - {today}
+        #### Bug Fixes
+        - bug fix 1 - ([{commit_four_short}](https://github.com/test/test/commit/{commit_four})) - Tom
+        #### Features
+        - feature 2 - ([{commit_three_short}](https://github.com/test/test/commit/{commit_three})) - Tom
+        #### Miscellaneous Chores
+        - (**version**) 2.0.0 - ([{commit_five_short}](https://github.com/test/test/commit/{commit_five})) - Tom
+
+        - - -
+
+        ## [1.0.0](https://github.com/test/test/compare/{init}..1.0.0) - {today}
+        #### Features
+        - feature 1 - ([{commit_two_short}](https://github.com/test/test/commit/{commit_two})) - Tom
+        - (**scope1**) start - ([{commit_one_short}](https://github.com/test/test/commit/{commit_one})) - Tom
+        #### Miscellaneous Chores
+        - init - ([{init_commit}](https://github.com/test/test/commit/{init})) - Tom
+
+
+        ",
+        today = today,
+        init = &init,
+        init_commit = &init[0..7],
+        commit_one = &commit_one,
+        commit_one_short = &commit_one[0..7],
+        commit_two = &commit_two,
+        commit_two_short = &commit_two[0..7],
+        commit_three = &commit_three,
+        commit_three_short = &commit_three[0..7],
+        commit_four = &commit_four,
+        commit_four_short = &commit_four[0..7],
+        commit_five = &commit_five,
+        commit_five_short = &commit_five[0..7],
+    );
+    Ok(())
+}
+
+#[sealed_test]
 fn should_ignore_merge_commit() -> Result<()> {
     // Arrange
     git_init()?;
